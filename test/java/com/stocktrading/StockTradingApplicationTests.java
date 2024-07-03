bd9d26ff7eca604f6a17a0907722bb741d378e3d
rlaalsrl0922 <rlaalsrl0922@khu.ac.kr>
2024-07-03 21:27:39 +0900

    init

---
+++ test/java/com/stocktrading/StockTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocktrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
