487bcbbd7104903f24ccd640f3af43feecb2c538
rlaalsrl0922 <rlaalsrl0922@khu.ac.kr>
2024-07-07 14:02:14 +0900

    croller modified

--- croller.py
+++ croller.py
@@ -1,41 +1,54 @@
-#import modules
+import tickertick as tt
+import tickertick.query as query
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import yfinance
-import FinanceDataReader as fdr
-from datetime import datetime, date
+from soup2dict import convert
 import json
-from os.path import exists
+from datetime import datetime
 
+
+def Timeconverter(x):
+    return datetime.fromtimestamp(x)
+
+def update_time(d):
+    if isinstance(d, dict):
+        for key, value in d.items():
+            if key == 'time':
+                d[key] = Timeconverter(d[key]//1000)  # 새로운 값으로 변경 (예시로 10을 사용)
+            elif isinstance(value, (dict, list)):
+                update_time(value)
+    elif isinstance(d, list):
+        for item in d:
+            update_time(item)
 
 def main():
-    
-    
-    ticker = input()
+    print("write ticker and number of newses")
 
-    aapl = yfinance.Ticker(ticker.upper())
+    ticker,count=tuple(map(str,input().split()))
 
-    print(aapl.news)
-    print(aapl.news[0]['link'])
-    sub = aapl.news
-    uuid,publisher,title,link=[],[],[],[]
-    for i in range(len(aapl.news)):
-        title.append(sub[i]['title'])
-        link.append(sub[i]['link'])
-        uuid.append(sub[i]['uuid'])
-        publisher.append(sub[i]['publisher'])
+    url = f"https://api.tickertick.com/feed?q=z:{ticker}&n={count}"
 
-    ''' Data Sample
-    'uuid': '96ade2ca-d763-3676-9a1f-f9b160794bb0'
-    'title': 'July 4th travel, S&P 500 broadening: Asking for a Trend'
-    'publisher': 'Yahoo Finance Video'
-    'link': 'https://finance.yahoo.com/video/july-4th-travel-p-500-215641022.html'
-    '''    
+    response = requests.get(url)
 
-    #Maximum 8
-    print(title)
-    print(link)
 
-if __name__ == "__main__":
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        dict_result = convert(soup)
+        navigable_string = dict_result["navigablestring"][0]
+        parsed_data = json.loads(navigable_string)
+
+        update_time(parsed_data)
+
+
+        # JSON 파일로 저장
+        with open('output.json', 'w', encoding='utf-8') as output_file:
+            json.dump(parsed_data, output_file, default=str,indent=2, ensure_ascii=False)
+
+
+    else : 
+        print(response.status_code)
+
+
+if __name__ =="__main__":
     main()
