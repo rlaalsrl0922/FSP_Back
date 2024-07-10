2e9c406c8741d4c6297526aca1c3988e81327169
rlaalsrl0922 <rlaalsrl0922@khu.ac.kr>
2024-07-10 18:20:20 +0900

    plus minus seperate

--- Topcroller.py
+++ Topcroller.py
@@ -31,16 +31,17 @@ def main():
                 data[idx]['price'] = price
                 idx+=1
 
-
-        idx = 0
-        for td in soup.find_all('td', class_='rh-sm'):
-            today = td.find('span').text.strip()
+        td_tags = soup.find_all('td', class_='rh-sm')
+        idx=0
+        for td_tag in td_tags:
+            span_tag = td_tag.find('span')
+            if span_tag:
+                if 'percentage-red' in span_tag['class']:
+                    today = '-' + span_tag.get_text(strip=True).split('-')[-1]
+                elif 'percentage-green' in span_tag['class']:
+                    today = '+' + span_tag.get_text(strip=True).split('+')[-1]
             data[idx]['today'] = today
             idx += 1
-
-
-
-
 
         with open('company_data.json', 'w', encoding='utf-8') as json_file:
             json.dump(data, json_file, indent=4, ensure_ascii=False)
